77711d6cfa61e79d06279f2cea7f613ffcf9c556
pxfnc <41906308+pxfnc@users.noreply.github.com>
2023-08-15 23:26:59 +0900

    apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Infinite from "./Infinite";
 function App() {
   return (
     <section className="w-full">
-      <h1 className="text-4xl py-8 px-2 font-bold">use paging and infinity scroll</h1>
+      <h1 className="text-4xl py-8 px-2 font-bold">
+        use paging and infinity scroll
+      </h1>
       <div className="flex p-2">
         <Paging className="flex-1" />
         <Infinite className="flex-1" />
